59872d417edb2381e87bea0f4bd23258615f5783
elchapa24 <gabo3392522554@gmail.com>
2024-11-24 16:53:29 -0300

    Update puntoB.sh

--- puntoB.sh
+++ puntoB.sh
@@ -1,9 +1,9 @@
-sudo ssh-keygen
-cat .ssh/id_rsa.pub
-vim .ssh/authorized_keys
+genero una llave en la maquina desarrollo - sudo ssh-keygen
+miro la llave publica en - cat .ssh/id_rsa.pub
+pego la llave publica en - vim .ssh/authorized_keys
 
-ssh vagrant@196.168.56.9
+me conecto desde el node con - ssh vagrant@192.168.56.9
 
-sudo apt update
-sudo apt install apache2
+actualizo los paquetes - sudo apt update
+instalo apache - sudo apt install apache2
 
