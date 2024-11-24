66948661135d7d7cc548e8302f4666bced1dce9c
elchapa24 <gabo3392522554@gmail.com>
2024-11-24 17:29:12 -0300

    Update puntoA.sh

--- puntoA.sh
+++ puntoA.sh
@@ -15,3 +15,16 @@ sudo lvcreate -L 2G vgAdmin -n lvAdmin
 sudo lvcreate -L 1.5G vgAdmin -n lvAdmin
 sudo lvcreate -L 1G vgAdmin -n lvAdmin
 sudo lvcreate -L 0.8G vgAdmin -n lvAdmin
+
+sudo mkfs.ext4 /dev/mapper/vgDevelopers-lvDevelopers
+sudo mkfs.ext4 /dev/mapper/vgDevelopers-lvTesters
+sudo mkfs.ext4 /dev/mapper/vgDevelopers-lvDevops
+sudo mkfs.ext4 /dev/mapper/vgAdmin-lvAdmin
+sudo mkdir /mnt/lvDevelopers
+sudo mkdir /mnt/lvTesters
+sudo mkdir /mnt/lvDevops
+sudo mkdir /mnt/lvAdmin
+sudo mount /dev/mapper/vgDevelopers-lvDevelopers /mnt/lvDevelopers
+sudo mount /dev/mapper/vgDevelopers-lvTesters /mnt/lvTesters
+sudo mount /dev/mapper/vgDevelopers-lvDevops /mnt/lvDevops
+sudo mount /dev/mapper/vgAdmin-lvAdmin /mnt/lvAdmin
