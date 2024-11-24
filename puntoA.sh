c44c9c318a566639850b3f5e7f51843d615fabf1
elchapa24 <gabo3392522554@gmail.com>
2024-11-24 17:14:00 -0300

    Update puntoA.sh

--- puntoA.sh
+++ puntoA.sh
@@ -1,8 +1,17 @@
 sudo fdisk /dev/sdc
-sudo pvcreate /dev/sdc2 /dev/sdc3 /dev/sdc5 /dev/sdc6
+
+sudo pvcreate /dev/sdc5 /dev/sdc6
+sudo pvcreate /dev/sdc2 /dev/sdc3
+
 sudo vgcreate vgAdmin /dev/sdc2 /dev/sdc3
 sudo vgcreate vgDevelopers /dev/sdc5 /dev/sdc6
-sudo lvcreate -L 2G -n lyAdmin vgAdmin
-sudo lvcreate -L 1G -n lvDevelopers vgDevelopers
-sudo lvcreate -L 1G -n lvTesters vgDevelopers
-sudo lvcreate -L 1G -n lvDevops vgDevelopers
+
+sudo lvcreate -L 1G vgDevelopers -n lvDevelopers
+sudo lvcreate -L 1G vgDevelopers -n lvTesters
+sudo lvcreate -L 1G vgDevelopers -n lvDevops
+sudo lvcreate -L 0.8G vgDevelopers -n lvDevops
+
+sudo lvcreate -L 2G vgAdmin -n lvAdmin
+sudo lvcreate -L 1.5G vgAdmin -n lvAdmin
+sudo lvcreate -L 1G vgAdmin -n lvAdmin
+sudo lvcreate -L 0.8G vgAdmin -n lvAdmin
